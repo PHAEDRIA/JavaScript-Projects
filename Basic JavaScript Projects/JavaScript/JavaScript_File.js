51fa28516d10e095f49e55b19e08f019d17dd033
PHAEDRIA <phaedriathayer@gmail.com>
2023-01-19 13:56:09 -0800

    updates

---
+++ Basic JavaScript Projects/JavaScript/JavaScript_File.js
@@ -0,0 +1,17 @@
+//main.js
+
+//window.alert method
+window.alert("OI OI!"); 
+
+//document.write method
+document.write("I looked at my cat, \Boujie don't you dare think about it! Or I'll tell dad!\"<br> \"he knocked the vase off the table looking at me smirking"); //trying out vairaible, not going so well
+
+//creating two varaibles
+var Sent1 = "This is a begining of a string";
+var Sent2 = " and this is the end of the string"
+
+//displayinng the varriables using the document.write method
+document.write(Sent1 + Sent2);
+
+//an exrpesssion
+document.write(38 * 84);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>   
+    <meta charset="utf-8">
+        <script src="JavaScript_File.js"></script>
+</head>
+<body>
+    <script>
+        alert ("OI OI!");
+    </script>      
+</body>
+</html>
